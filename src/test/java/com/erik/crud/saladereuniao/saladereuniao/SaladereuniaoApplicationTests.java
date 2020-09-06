f420d25173d62a12ea50049bdaace243419bc8b1
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2020-09-06 14:11:45 -0300

    Iniciando o projeto

---
+++ src/test/java/com/erik/crud/saladereuniao/saladereuniao/SaladereuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erik.crud.saladereuniao.saladereuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaladereuniaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
